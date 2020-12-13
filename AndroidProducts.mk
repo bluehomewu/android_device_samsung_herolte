8184a6199e777f7aca775faf939de18701f8e89c
bluehomewu <bluehome.wu@gmail.com>
2020-12-14 00:05:23 +0800

    herolte: Init Havoc

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_herolte.mk
+    $(LOCAL_DIR)/havoc_herolte.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_herolte-user \
-    lineage_herolte-userdebug \
-    lineage_herolte-eng
+    havoc_herolte-user \
+    havoc_herolte-userdebug \
+    havoc_herolte-eng
--- havoc_herolte.mk
+++ havoc_herolte.mk
@@ -23,11 +23,11 @@ $(call inherit-product, device/samsung/herolte/device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := herolte
-PRODUCT_NAME := lineage_herolte
+PRODUCT_NAME := havoc_herolte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G930F
 PRODUCT_MANUFACTURER := samsung
